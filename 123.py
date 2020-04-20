5a64f65dc09a89aae8dc4a7afaffd0a8df37dddd
Nick <niczz9797@gmail.com>
2020-04-20 19:59:49 +0300

    костыль №2.5 + блок на выбор mat файлов

--- 123.py
+++ 123.py
@@ -1,4 +1,25 @@
-s = ['1', '2', '33']
+# import collections
+#
+# Val = collections.namedtuple('Val', 'capacity flux inductivity')
+# print(Val)
+# val = Val(capacity=20, flux=30, inductivity=0)
+# print(val)
+# val._asdict()
+#
+# print(getattr(val,'capacity'))
 
-print([int(i) for i in s])
-print(list(map(int, s)))
+import yaml
+
+x = """
+electron: True
+proton: 
+        low_en: 1
+        hight_en: 2
+pozitron:
+        a: 1
+        b: 2
+"""
+
+
+files = yaml.load(x)
+print(files['proton'])
